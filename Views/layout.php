93d1c60f192cf2067449aade2d3191a2c19145fa
basic-app <dev@basic-app.com>
2026-04-06 23:07:16 +0300

    n/a

--- Views/layout.php
+++ Views/layout.php
@@ -8,8 +8,6 @@ SiteEvents::registerAssets($theme->head, $theme->beginBody, $theme->endBody);
 
 $params = SiteEvents::mainLayout([
     'mainMenu' => $this->data['mainMenu'] ?? [],
-    'actionMenu' => $this->data['actionMenu'] ?? [],
-    'breadcrumbs' => $this->data['breadcrumbs'] ?? [],
     'content' => $content,
     'title' => $this->data['title'] ?? null
 ]);
